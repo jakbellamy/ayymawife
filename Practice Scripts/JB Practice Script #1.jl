838a03d1e8777d276f983a6927298c9f2ccb3c17
Kob <43860560+jakbellamy@users.noreply.github.com>
2021-12-30 12:35:20 -0500

    Started some notes got bored

--- Practice Scripts/JB Practice Script #1.jl
+++ Practice Scripts/JB Practice Script #1.jl
@@ -1,3 +1,15 @@
+# Welcome! I'm testing some stuff out
+
+newblock = ()->println("-"^40) # This is just to define a new section for te console
+
+newblock() # Block 1: Unicode Support
+println("Block 1: Unicode Support!")
+println("\tauto-completed using \\sigma")
+println("\tσ=2.5")
+
 σ = 2.5
 println(σ)
-println()
+
+newblock() # Block 2: Some Cool Arithmetic stuff
+println("Block 2: Arithmetic!")
+println("I got bored and this takes too long i'm moving on and not taking notes")
